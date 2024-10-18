7beda3d204462b85494c62db2981abc6a17be9ff
12216291 <e12216291@student.tuwien.ac,at>
2024-10-18 11:04:29 +0200

    neue Klassen angelegt

---
+++ Aufgabe1-3/src/minBuilding.java
@@ -0,0 +1,26 @@
+public class minBuilding implements Building{
+    @Override
+    public void build(int apartments, int inhabitants) {
+
+    }
+
+    @Override
+    public void age() {
+
+    }
+
+    @Override
+    public boolean deconstruct() {
+        return false;
+    }
+
+    @Override
+    public void renovate() {
+
+    }
+
+    @Override
+    public int susIndex() {
+        return 0;
+    }
+}
