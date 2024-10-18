787fbec4e387a7d183b181d1352bcbfb529a34ba
12216291 <e12216291@student.tuwien.ac,at>
2024-10-18 11:10:37 +0200

    neue Klassen angelegt

---
+++ Aufgabe1-3/src/Simulation.java
@@ -0,0 +1,6 @@
+public class Simulation {
+    /** this class performs the simulation of the lifespan of the buildings. Therefore, it offers all
+     * necessary methods to start and end a simulation. A simulation can be started in any runnable class.
+     * Its methods are called in the "Test" class.
+     */
+}
