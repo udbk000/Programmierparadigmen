09b971153393feeb3f1113960f3fe385b22f0376
andrii <andrew.makarenko.04@gmail.com>
2024-10-20 11:51:21 -0700

    Simulation Klasse hinzugefügt

--- Aufgabe1-3/src/Simulation.java
+++ Aufgabe1-3/src/Simulation.java
@@ -3,6 +3,76 @@
  * of "Building".
  * Its methods are called in the "Test" class in order to start a simulation.
  */
+import java.util.Random;
 public class Simulation {
 
+    private final Building building;
+    private final int maxYears;
+    private final Random random;
+    private final String buildingType;
+
+    public Simulation(Building building, int maxYears) {
+        this.building = building;
+        this.maxYears = maxYears;
+        this.random = new Random();
+        if(building instanceof minBuilding)
+            buildingType = "minimal building";
+        else if (building instanceof ecoBuilding)
+            buildingType = "eco building";
+        else
+            buildingType = "high quality building";
+    }
+
+    //Methode mit einer schleife, die die simulation maxYears lang laufen lässt
+    public void runSimulation() {
+        System.out.println("Starting simulation for " + maxYears + " years for the " + buildingType + "...");
+
+        for (int year = 1; year <= maxYears; year++) {
+            if (building.isDeconstructed()) {
+                System.out.println("The " + buildingType + " has been deconstructed after " + (year - 1) + " years.");
+                break;
+            }
+            System.out.println("\nYear " + year + "of the " + buildingType + ":");
+            building.ageOneYear();
+            simulateRandomEvents();
+            if (random.nextInt(100) < 15) {
+                building.renovate();
+            }
+        }
+
+        if (!building.isDeconstructed()) {
+            System.out.println("\nThe building reached its maximum lifespan of " + maxYears + " years.");
+            building.deconstruct(); // Deconstruct after maximum age is reached
+        }
+
+        printSimulationResults();
+    }
+
+    //Methode für eine zufällige erzeugung einer katastrophe
+    private void simulateRandomEvents() {
+        int eventChance = random.nextInt(100);
+        if (eventChance < 3) { // 3% chance for a catastrophic event
+            int eventType = random.nextInt(3);
+            switch (eventType) {
+                case 0:
+                    building.fire();
+                    break;
+                case 1:
+                    building.earthquake();
+                    break;
+                case 2:
+                    building.flooding();
+                    break;
+            }
+        }
+    }
+
+    //Methode für ausgabe der simulationsergebnisse
+    public void printSimulationResults() {
+        System.out.println("\n--- Simulation Results: " + buildingType + " ---");
+        building.printAvgStats();
+        building.printCostsByDecade();
+        building.printSatisfactionByDecade();
+        System.out.printf("Sustainability Index: %d%n", building.susIndex());
+    }
 }
