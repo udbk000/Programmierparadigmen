3d10ab25c303a9b496493929d4774c484e7cb5c7
udbk000 <usaidaldbk@gmail.com>
2024-10-20 12:40:25 +0200

    Bearbeitung der Klasse ecoBuilding und des Interfaces Building; einarbeitung der naturkatastrophen

--- Aufgabe1-3/src/ecoBuilding.java
+++ Aufgabe1-3/src/ecoBuilding.java
@@ -7,8 +7,6 @@
  */
 public class ecoBuilding implements Building{
 
-    private final int AGE_EXPECTATION = 50;
-
     /**
      * Variable co2Emissions defines the amount of CO2 emissions produced by an ecoBuilding in tons.
       */
@@ -49,6 +47,11 @@ public class ecoBuilding implements Building{
     private double condition;
 
     /**
+     * This variable is set true when the building is deconstructed. If true, methods act accodringly.
+     */
+    private boolean isDeconstructed;
+
+    /**
      * This constructor is called to construct an ecological building. The variables being set define the minimal cost of
      * every aspect (i.e. variable) of this building. These aspects are made dependent on the size of the building, therefore
      * the amount of people living in this building. The initial values are considered the values of each variable at the moment
@@ -63,52 +66,135 @@ public class ecoBuilding implements Building{
         satisfaction = 55 + (Math.random() * (85-55)+1); //55= min. initial satisfaction, 85= max. initial satisfaction
         co2Emissions = 30 + inhabitants*2; //30 = emissions through construction, 2= emission per inhabitant
         condition = 100.0;
+        isDeconstructed = false;
     }
 
     @Override
     public void ageOneYear() {
-        cost += 2500*inhabitants;
-        waste += 0.3*inhabitants;
-        age++;
-        if(age >= AGE_EXPECTATION){
-            int x = (int) (Math.random()*100);
-            if(x%3 == 0){
-                this.deconstruct();
-            } else if(x%3 == 1){
-                this.renovate();
-            } else {
-                if(satisfaction > 0){
-                    satisfaction = satisfaction - satisfaction*0.2;
+        if(isDeconstructed){
+            System.out.println("This building has been deconstructed.");
+        } else {
+            cost += 2500 * inhabitants;
+            waste += 0.3 * inhabitants;
+            age++;
+            int AGE_EXPECTATION = 50;
+            if (age >= AGE_EXPECTATION) {
+                int x = (int) (Math.random() * 100);
+                if (x % 3 == 0) {
+                    this.deconstruct();
+                } else if (x % 3 == 1) {
+                    this.renovate();
+                } else {
+                    if (satisfaction > 0) {
+                        satisfaction = satisfaction - satisfaction * 0.2;
+                    }
                 }
             }
-        }
-        if(satisfaction > 0){
-            satisfaction -= 1;
-        }
-        co2Emissions += inhabitants*2;
-        condition -= 5;
-        if(condition < 50){
-            int x = (int) (Math.random()*100);
-            if(x % 5 == 0){
-                this.deconstruct();
-            } else {
-                this.renovate();
+            if (satisfaction > 0) {
+                satisfaction -= 1;
+            }
+            co2Emissions += inhabitants * 2;
+            condition -= 5;
+            if (condition < 50) {
+                int x = (int) (Math.random() * 100);
+                if (x % 5 == 0) {
+                    this.deconstruct();
+                } else {
+                    this.renovate();
+                }
             }
         }
     }
 
     @Override
     public boolean deconstruct() {
-        return false;
+        if(isDeconstructed){
+            System.out.println("This building has already been deconstructed.");
+            return false;
+        }
+        System.out.println("Deconstructing the ecological building...");
+        waste += 10 + 0.5 * inhabitants;
+        co2Emissions += 50+ inhabitants * 3;
+        satisfaction = 0;
+        condition = 0;
+        isDeconstructed = true;
+        return true;
     }
 
     @Override
     public void renovate() {
+        if(isDeconstructed){
+            System.out.println("This building has been deconstructed and cannot be renovated.");
+        } else {
+            System.out.println("Renovating the building...");
+            cost += 10000 + 1500 * inhabitants;
+            waste += 2 + 0.2*inhabitants;
+            co2Emissions += 20 + inhabitants * 1.5;
+            satisfaction = Math.min(satisfaction +20, 100);
+            condition = Math.min(condition + 50, 100);
+        }
 
     }
 
     @Override
     public int susIndex() {
-        return 0;
+        double index = (satisfaction / cost) * (1/co2Emissions) * (1/waste);
+        return (int) (index * 1000);
     }
+
+    @Override
+    public void fire() {
+        if(isDeconstructed){
+            System.out.println("The building has been deconstructed. No fire can occur.");
+        }
+        else {
+            System.out.println("A fire has broken out in the ecological building.");
+            cost += 15000 + 2000*inhabitants;
+            co2Emissions += 50 + 5*inhabitants;
+            waste += 10 + 0.5*inhabitants;
+            satisfaction = Math.max(satisfaction-30, 0);
+            condition = Math.max(condition - 40, 0);
+
+            if(condition <= 15){
+                deconstruct();
+            }
+        }
+    }
+
+    @Override
+    public void earthquake() {
+        if(isDeconstructed){
+            System.out.println("This building has been deconstructed. No earthquake can occur.");
+        } else {
+            System.out.println("An earthquake has struck the building.");
+            cost += 25000+3000*inhabitants;
+            co2Emissions += 100 + 10*inhabitants;
+            waste += 15 + 1*inhabitants;
+            satisfaction = Math.max(satisfaction-40, 0);
+            condition = Math.max(condition-50, 0);
+            if(condition <= 15){
+                deconstruct();
+            }
+        }
+
+    }
+
+    @Override
+    public void flooding() {
+        if(isDeconstructed){
+            System.out.println("This building has been deconstructed. No flooding can occur.");
+        } else {
+            System.out.println("The building has been flooded.");
+            cost += 10000+1500*inhabitants;
+            co2Emissions += 30 + 3*inhabitants;
+            waste += 8 + 0.4*inhabitants;
+            satisfaction = Math.max(satisfaction-20, 0);
+            condition = Math.max(condition-30,0);
+            if(condition <= 15){
+                deconstruct();
+            }
+        }
+    }
+
+
 }
