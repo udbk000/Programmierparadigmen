f16b38c0f4759dfc2481aaeb1431c6f79bbf64cf
udbk000 <usaidaldbk@gmail.com>
2024-10-20 15:44:22 +0200

    hiQBuilding bearbeitet
    minBuilding bearbeitet

--- Aufgabe1-3/src/Test.java
+++ Aufgabe1-3/src/Test.java
@@ -1,4 +1,10 @@
-/** this class calls the beginning of the simulation, returns the results and is
+/**
+ * Wer hat was gemacht?
+ * Usaid Al-Dabak, 12216291: Interface Building, Klassen ecoBuilding, hiQBuilding und minBuilding zusammengestellt und implementiert.
+ */
+
+/**
+ * This class calls the beginning of the simulation, returns the results and is
  * the main point of interaction.
  */
 public class Test {
