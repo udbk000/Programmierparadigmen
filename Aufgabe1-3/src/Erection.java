d8dddcc8c3b67d6f62ecc730428cd1c1c11f822f
12216291 <e12216291@student.tuwien.ac,at>
2024-10-31 12:44:22 +0100

    Datenbank angelegt, Klassen angelegt

---
+++ Aufgabe1-3/src/Erection.java
@@ -0,0 +1,19 @@
+
+public class Erection {
+
+    private Szenario scenario;
+
+    private int inhabitants;
+
+    private Landscape landscape;
+
+    private int area;
+
+    public Erection(){
+        scenario = null;
+    }
+
+    public void setScenario(String scene){
+
+    }
+}
---
+++ Aufgabe1-3/src/Szenario.java
@@ -0,0 +1,17 @@
+public class Szenario {
+    private String name;
+
+    private float co2Emission;
+
+    private String material;
+
+    private float waste;
+
+    //per m2
+    private float costs;
+
+    public Szenario(){
+
+    }
+
+}
