a26733031969ac65a05a91b892340f7f0df54813
12216291 <e12216291@student.tuwien.ac,at>
2024-11-10 17:34:06 +0100

    Szenario und Construction: kommentare und assertions geschrieben
    3x GOOD OOP Kommentare
    1x GOOD prozedural Kommentare
    1x BAD prozedural Kommentare
    2x BAD OOP Kommentare
    geschrieben
    
    Klasse World erledigt (parallele Programmierung)
    
    Klasse FuncCSVReader implementiert
    (funktionale Programmierung)
    [nur 50 Zeilen]

---
+++ Aufgabe1-3/src/FuncCSVReader.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * STYLE:
+ * Diese Klasse ist funktional implementiert. Sie ersetzt die Klasse "CSVReader" durch eine funktionale Implementierung.
+ * Die Implementierung der java.util.function Bibliothek ermöglicht die Implementierung von Functions, wodurch das Paradigma
+ * in Java vollständig eingesetzt werden kann. Die Funktion createSzenario illustriert die Verwendung des Paradigmas.
+ */
+
+public class FuncCSVReader {
+    public List<Szenario> loadScenariosFromCSV(String filePath) {
+        List<Szenario> scenarios = new ArrayList<>();
+        String line;
+        String csvSplitBy = ",";
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(csvSplitBy);
+
+                String name = fields[0];
+                float co2Emission = Float.parseFloat(fields[1]);
+                String material = fields[2];
+                float waste = Float.parseFloat(fields[3]);
+                float constructionCosts = Float.parseFloat(fields[4]);
+                float mainCosts = Float.parseFloat(fields[5]);
+                int materialAge = Integer.parseInt(fields[6]);
+
+                Szenario scenario = new Szenario(name, co2Emission, material, waste, constructionCosts, mainCosts, materialAge, null);
+                scenarios.add(scenario);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading CSV file: " + e.getMessage());
+        }
+        return scenarios;
+    }
+
+    // Funktion, die eine Szene von der CSV-Zeile erstellt
+    Function<String, Szenario> createSzenario = line -> {
+        String[] fields = line.split(",");
+        String name = fields[0];
+        float co2Emission = Float.parseFloat(fields[1]);
+        String material = fields[2];
+        float waste = Float.parseFloat(fields[3]);
+        float constructionCosts = Float.parseFloat(fields[4]);
+        float mainCosts = Float.parseFloat(fields[5]);
+        int materialAge = Integer.parseInt(fields[6]);
+        return new Szenario(name, co2Emission, material, waste, constructionCosts, mainCosts, materialAge, null);
+    };
+
+
+}
+
