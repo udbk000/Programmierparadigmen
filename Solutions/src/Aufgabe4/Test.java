0319e3102c5fecfa3e425b7e3ae7c41e10f84128
udbk000 <usaidaldbk@gmail.com>
2024-11-24 17:06:26 +0100

    Modell der Beziehungen erarbeitet Typ Entity samt allen Untertypen implementiert

--- Solutions/src/Aufgabe4/Test.java
+++ Solutions/src/Aufgabe4/Test.java
@@ -5,4 +5,7 @@ package Aufgabe4;
  * Usaid Al-Dabak, 12216291: Modell der Untertypbeziehungen erarbeitet und durchdacht, Implementierung des Typs Entity und alle Untertypen von Entity
  * */
 public class Test {
+    public static void main(String[] args) {
+
+    }
 }
