04bfcf397b7188b65eb21e84b618c05b852af36c
stefanslanar <sslanar@gmx.at>
2024-12-01 00:57:50 +0100

    Path-Klasse angefangen

---
+++ Solutions/src/Aufgabe5/Path.java
@@ -0,0 +1,25 @@
+package Aufgabe5;
+
+import java.util.Objects;
+
+public class Path<X> implements Admin<X, Path<X>>, Iterable<X> {
+
+    private final MyList<X> elements;
+
+    public Path() {
+        this.elements = new MyList<>();
+    }
+
+    private Path(MyList<X> elements) {
+        this.elements = new MyList<>(elements);
+    }
+
+    public Path<X> add(X x) {
+        Objects.requireNonNull(x);
+        if (!elements.contains(x)) {
+            elements.add(x);
+            return new Path<>(this.elements);
+        }
+        return this;
+    }
+}
