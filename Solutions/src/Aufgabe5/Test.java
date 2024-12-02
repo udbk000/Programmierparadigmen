cc54c7de41111888e4be8cecda9e6049af67e180
12216291 <e12216291@student.tuwien.ac,at>
2024-12-02 11:09:47 +0100

    wer hat was gemacht eingetragen

--- Solutions/src/Aufgabe5/Test.java
+++ Solutions/src/Aufgabe5/Test.java
@@ -2,7 +2,7 @@ package Aufgabe5;
 
 /**
  * Wer hat was gemacht?
- * Usaid Al-Dabak, 12216291: Admin, Approvable (+Zusicherungen),
+ * Usaid Al-Dabak, 12216291: Admin, Approvable, ApprovableSet, ApSet, AdminSet, MyIterator, MyList, MyListIterator, Node implementiert (und entsprechend die Kommentare in diesen Klassen verfasst)
  */
 public class Test {
     public static void main(String[] args) {
