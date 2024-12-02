04bc85b1d0e07deca9a837b1d261635efdc523c2
stefanslanar <sslanar@gmx.at>
2024-12-02 11:30:13 +0100

    eingetragen, was ich gemacht hab

--- Solutions/src/Aufgabe5/Test.java
+++ Solutions/src/Aufgabe5/Test.java
@@ -3,6 +3,7 @@ package Aufgabe5;
 /**
  * Wer hat was gemacht?
  * Usaid Al-Dabak, 12216291: Admin, Approvable, ApprovableSet, ApSet, AdminSet, MyIterator, MyList, MyListIterator, Node implementiert (und entsprechend die Kommentare in diesen Klassen verfasst)
+ * Stefan Slanar, 12327076: Path-Klasse, mitgearbeitet an Listenimplementierung und Debugging
  */
 public class Test {
     public static void main(String[] args) {
