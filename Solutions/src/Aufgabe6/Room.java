4dcfdd1364105654b1aafb56628000c179b09473
12216291 <e12216291@student.tuwien.ac,at>
2024-12-06 14:27:15 +0100

    room beschrieben

--- Solutions/src/Aufgabe6/Room.java
+++ Solutions/src/Aufgabe6/Room.java
@@ -1,5 +1,9 @@
 package Aufgabe6;
 
+/**
+ * This interface defines a Room with its functionality. Rooms can be subdivided into several types of Rooms, which will
+ * be defined by subtypes of this interface. A room has
+ */
 public interface Room {
     void call();
 
