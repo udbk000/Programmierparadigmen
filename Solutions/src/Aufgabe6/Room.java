014786a32534e6c83a0dc1f142819a84d265a809
12216291 <e12216291@student.tuwien.ac,at>
2024-12-06 18:11:21 +0100

    Room und SideRoom implementiert

--- Solutions/src/Aufgabe6/Room.java
+++ Solutions/src/Aufgabe6/Room.java
@@ -1,10 +1,68 @@
 package Aufgabe6;
 
 /**
- * This interface defines a Room with its functionality. Rooms can be subdivided into several types of Rooms, which will
- * be defined by subtypes of this interface. A room has
+ * This interface describes a room in an office with its functionality. Rooms can be subdivided into several types of Rooms, which will
+ * be defined by subtypes of this interface. A room has several different purposes. One aspect that is applicable to each and
+ * every room is its area. Rooms that implement this interface are always viewed as either rectangular or square-shaped in order
+ * to keep the simulation simple and easily understandable. Check all method description in order to understand how method calls behave in different
+ * subtypes of this interface.
  */
 public interface Room {
-    void call();
+    /**
+     * This method calculates the area of the entire room.
+     * @return Area in m^2 as a floating point number. Can only be positive or zero.
+     */
+    float area();
+
+    /**
+     * This method returns the length in m for every room it is called in.
+     * @return the length of this room in m, length >=0
+     */
+    float getLength();
+
+    /**
+     * This method returns the width in m for every room it is called in.
+     * @return the width of this room in m, width >= 0
+     */
+    float getWidth();
+
+    /**
+     * Usable Rooms get designated names. This method makes it possible to find out a room's name.
+     * @return a String which represents the name of the usable room, null else
+     */
+    String getName();
+
+    /**
+     * Given a usable room with windows, a call of this method returns the surface area of all windows combined inside of this room.
+     * The unit is m^2.
+     * @return a floating point number >=0 if the Room is a WindowRoom, -1 else
+     */
+    float getWindowArea();
+
+    /**
+     * Given a usable room without windows, a call of this method returns the luminous flux in Lumen (lm).
+     * @return the luminous flux in Lumen if the Room is a LightRoom, -1 else
+     */
+    int getLumen();
+
+    /**
+     * Given a usable room, this method will change the room's designated purpose. Usable rooms can either be used as a bureau or as storage rooms.
+     * Therefore, this method acts a switch between these two purposes. If the method is called in a room that is not a usable room, the method will not do anything.
+     */
+    void changePurpose();
+
+    /**
+     * Given a usable room with its purpose being a bureau, this method will return an int which represents the amount of
+     * different workplaces inside this room. A call of this method in a usable room that does not serve as a bureau will always return 0.
+     * @return an integer >=0 counting the amount of distinct workplaces, -1 if the room is not usable
+     */
+    int getWorkplace();
+
+    /**
+     * Given a usable room with its purpose being a storage room, this method will return the volume of this room in m^3.
+     * A call of this method in a usable room that does not serve as a storage unit will always return 0.
+     * @return a floating point number >=0 representing the max volume available for storage purposes in this room, -1 if the room is not usable
+     */
+    float getStorage();
 
 }
---
+++ Solutions/src/Aufgabe6/SideRoom.java
@@ -0,0 +1,96 @@
+package Aufgabe6;
+
+/**
+ * This class describes a "SideRoom", which refers to a secondary room which does not serve a specific purpose.
+ * There is not much information needed from such rooms. Examples of such rooms in an office could be a hallway, restrooms, etc.
+ * The only information that these rooms provide is the area of such room.
+ */
+public class SideRoom implements Room {
+    private float length; //the length of this room, length >= 0
+    private float width; //the width of this room, width >= 0
+
+    /**
+     * This constructor initializes this as a SideRoom. The only necessary information for a SideRoom are its length and with.
+     * @param l defines the length of this room, must be a floating point number, l >= 0
+     * @param w defines the width of this room, must be a floating point number, w >= 0
+     */
+    public SideRoom(float l, float w){
+        length = l;
+        width = w;
+    }
+
+    /**
+     * This method calculates the area of this SideRoom.
+     * @return the area of this SideRoom in m2. Can only be positive or zero.
+     */
+    @Override
+    public float area() {
+        return length*width;
+    }
+
+    @Override
+    public float getLength() {
+        return length;
+    }
+
+    @Override
+    public float getWidth() {
+        return width;
+    }
+
+
+    /**
+     * This method is not supported for a SideRoom.
+     * @return null
+     */
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    /**
+     * This method is not supported for a SideRoom.
+     * @return -1
+     */
+    @Override
+    public float getWindowArea() {
+
+        return -1;
+    }
+
+    /**
+     * This method is not supported for a SideRoom.
+     * @return -1
+     */
+    @Override
+    public int getLumen() {
+
+        return -1;
+    }
+
+    /**
+     * This method is not supported for a SideRoom.
+     */
+    @Override
+    public void changePurpose() {
+        ;
+    }
+
+    /**
+     * This method is not supported for a SideRoom.
+     * @return -1
+     */
+    @Override
+    public int getWorkplace() {
+        return -1;
+    }
+
+    /**
+     * This method is not supported for a SideRoom.
+     * @return -1
+     */
+    @Override
+    public float getStorage() {
+        return -1;
+    }
+}
