e024f8424e1a3b19b375d24023f845bfc086ffaf
udbk000 <usaidaldbk@gmail.com>
2024-12-07 19:22:36 +0100

    implementations

--- Solutions/src/Aufgabe6/Test.java
+++ Solutions/src/Aufgabe6/Test.java
@@ -2,7 +2,7 @@ package Aufgabe6;
 
 /**
  * Wer hat was gemacht?
- * Usaid Al-Dabak, 12216291: Room, SideRoom, UsableRoom, WindowRoom, LightRoom, OfficeInt, OfficeGen (Implementierung und Dokumentation)
+ * Usaid Al-Dabak, 12216291: Room, SideRoom, UsableRoom, WindowRoom, LightRoom, OfficeInt, OfficeGen, BuildingInt, CompanyInt (Implementierung und Dokumentation)
  */
 public class Test {
     public static void main(String[] args) {
