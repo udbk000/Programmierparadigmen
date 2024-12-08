6d9aadd1d741c0b567a28307a3ee5e94d58fd785
stefanslanar <sslanar@gmx.at>
2024-12-08 22:56:02 +0100

    was ich gemacht habe

--- Solutions/src/Aufgabe6/Test.java
+++ Solutions/src/Aufgabe6/Test.java
@@ -3,6 +3,7 @@ package Aufgabe6;
 /**
  * Wer hat was gemacht?
  * Usaid Al-Dabak, 12216291: Room, SideRoom, UsableRoom, WindowRoom, LightRoom, OfficeInt, OfficeGen, BuildingInt, CompanyInt (Implementierung und Dokumentation)
+ * Stefan Slanar, 12327076: RoomList, RoomNode, BuildingList, BuildingNode, OfficeList, OfficeNode, IterableList, LinkedListIterator
  */
 public class Test {
     public static void main(String[] args) {
