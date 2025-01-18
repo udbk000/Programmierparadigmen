338d3f71b9aefa8a7ff10d1bbf6b3c8447a3e3c1
udbk000 <usaidaldbk@gmail.com>
2025-01-18 18:32:51 +0100

    Aufgabe9

--- Solutions/src/Aufgabe9/Test.java
+++ Solutions/src/Aufgabe9/Test.java
@@ -1,5 +1,12 @@
 package Aufgabe9;
 
+/*
+Wer hat was gemacht?
+Gruppenmitglied 1: Design und Implementierung der Datenstrukturen für das Wegenetz und Personen.
+Gruppenmitglied 2: Entwicklung der Logik für die Bewegungen und die Synchronisation der Threads.
+Gruppenmitglied 3: Umsetzung der Interprozesskommunikation (Serialisierung/Deserialisierung) und des Testprogramms.
+ */
+
 // Beispiel für die Verwendung
 public class Test {
     public static void main(String[] args) {
