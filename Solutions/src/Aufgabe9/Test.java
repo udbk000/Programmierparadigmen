3301d1c7b148c232945b68f1cc858cfb2932745a
stefanslanar <sslanar@gmx.at>
2025-01-20 01:44:30 +0100

    mit Vorsicht genießen

--- Solutions/src/Aufgabe9/Test.java
+++ Solutions/src/Aufgabe9/Test.java
@@ -1,9 +1,8 @@
 package Aufgabe9;
-/**https://chatgpt.com/share/678be650-2498-800c-86a6-79bbfc7e3fb0
 /*
 Wer hat was gemacht?
 Gruppenmitglied 1: Design und Implementierung der Datenstrukturen für das Wegenetz und Personen.
-Gruppenmitglied 2: Entwicklung der Logik für die Bewegungen und die Synchronisation der Threads.
+12327076, Stefan Slanar: Entwicklung der Logik für die Bewegungen und die Synchronisation der Threads.
 Gruppenmitglied 3: Umsetzung der Interprozesskommunikation (Serialisierung/Deserialisierung) und des Testprogramms.
  */
 
